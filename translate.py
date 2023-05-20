e689544ad850e538e1f026527c6b80a48a05e59a
Archie McKenzie <archiem@princeton.edu>
2023-05-20 17:08:34 +0100

    env update

--- translate.py
+++ translate.py
@@ -3,6 +3,22 @@
 # Author: Archie McKenzie 
 # © 2023, MIT License
 
-# ----- IMPORTS ----- #
+# ----- SETUP ----- #
+
+# Get the config file
+import json
+config = json.load(open('config.json', 'r'))
+
+# Load the environment variables from .env
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 import openai
+openai.api_key = os.getenv("API_KEY") or config.api_keys.OPENAI_API_KEY
+
+# ----- CHECKS ----- #
+
+# Calculate the number of tokens
+
